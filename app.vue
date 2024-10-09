21eb9bf4db6872743fb1268b0c66e7a67a595377
Don <don.puerto@hotmail.com>
2024-10-09 20:28:32 +0800

    ..

---
+++ app.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+import { ConfigProvider } from 'radix-vue';
+const useIdFunction = () => useId() || '';
+</script>
+<template>
+  <div>
+    <ConfigProvider :use-id="useIdFunction">
+      <NuxtLayout>
+        <NuxtPage />
+      </NuxtLayout>
+    </ConfigProvider>
+    
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<script lang="ts" setup>
+
+</script>
+<template>
+  <div>
+    <h1>Hello World</h1>
+    <UiButton>Click me</UiButton>
+  </div>
+</template>
+
